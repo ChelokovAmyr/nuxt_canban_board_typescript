e430102fcf21ece2bd637a9d2f13d38c33f224ef
Amyr <44586350+ChelokovAmyr@users.noreply.github.com>
2025-09-01 10:49:40 +0300

    tailwind installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   modules: ['@pinia/nuxt'],
     typescript: {
     typeCheck: true
-  }
+  },
 })
